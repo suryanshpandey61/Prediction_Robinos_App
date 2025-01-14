f697e48b4bb7288b48aa33b2cd32f81280838c79
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-01-14 22:35:54 +0530

    Wallet in Portfolio Updated

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -4,7 +4,8 @@ import Sidebar from "@/components/sidebar";
 import { FaWallet } from "react-icons/fa";
 import RobinoLogo from "../assets/token_40px.png";
 import Image from "next/image";
-import USDM from "../assets/usdm.png"
+import USDM from "../assets/usdm.png";
+import { FaFlag } from "react-icons/fa6";
 
 const Portfolio: React.FC = () => {
   return (
@@ -23,32 +24,63 @@ const Portfolio: React.FC = () => {
           <div className="text-white gap-x-4 flex items-center text-[20px] pl-4 pt-2">
             <FaWallet /> Wallet
           </div>
-         <div className="flex gap-x-2">
-         <div className="flex justify-between   rounded-lg bg-[#061230] w-[490px] pl-4 ml-4 mt-2 mb-4  items-center">
-            <div className="flex flex-col gap-x-2  py-[6px]">
-              <p className="text-white  items-center flex ">0.0000</p>
-              <div className="flex gap-x-1 items-center text-[14px] ">
-                <p className="font-bold text-slate-500 flex items-center">
-                  
-                  RBN
-                </p>
+          <div className="flex gap-x-2">
+            <div className="flex justify-between   rounded-lg bg-[#061230] w-[490px] pl-4 ml-4 mt-2 mb-4  items-center">
+              <div className="flex flex-col gap-x-2  py-[6px]">
+                <p className="text-white  items-center flex ">0.0000</p>
+                <div className="flex gap-x-1 items-center text-[14px] ">
+                  <p className="font-bold text-slate-500 flex items-center">
+                    RBN
+                  </p>
+                </div>
               </div>
+              <Image src={RobinoLogo} alt="" className="pr-2 w-[48px]" />
             </div>
-            <Image src={RobinoLogo} alt="" className="pr-2 w-[48px]" />
-          </div>  
-          <div className="flex justify-between   rounded-lg bg-[#061230] w-[490px] pl-4  mt-2 mb-4 mr-4  items-center">
-            <div className="flex flex-col gap-x-2  py-[6px]">
-              <p className="text-white  items-center flex ">0.0000</p>
-              <div className="flex gap-x-1 items-center text-[14px] ">
-                <p className="font-bold text-slate-500 flex items-center">
-                  
-                 USDM
-                </p>
+            <div className="flex justify-between   rounded-lg bg-[#061230] w-[490px] pl-4  mt-2 mb-4 mr-4  items-center">
+              <div className="flex flex-col gap-x-2  py-[6px]">
+                <p className="text-white  items-center flex ">0.0000</p>
+                <div className="flex gap-x-1 items-center text-[14px] ">
+                  <p className="font-bold text-slate-500 flex items-center">
+                    USDM
+                  </p>
+                </div>
               </div>
+              <Image src={USDM} alt="" className="pr-2 w-[48px]" />
             </div>
-            <Image src={USDM} alt="" className="pr-2 w-[48px]" />
-          </div> 
-         </div>
+          </div>
+        </div>
+
+        {/* versus event wala div  */}
+        <div className="bg-[#242555] border border-pink-900 mt-4 rounded-xl w-[980px]">
+          {/* wallet text with icon  */}
+          <div className="text-white gap-x-4 flex items-center text-[20px] pl-4 pt-2">
+            <FaFlag /> Versus Event Overview
+          </div>
+          <div className="flex gap-x-2">
+            <div className="flex justify-between   rounded-lg bg-[#061230] w-[490px] pl-4 ml-4 mt-2 mb-4  items-center">
+              <div className="flex flex-col gap-x-2  py-[6px]">
+                <p className=" text-slate-500 flex items-center">
+                  Versus Event Bought
+                </p>
+
+                <div className="flex gap-x-1 items-center text-[14px] ">
+                  <p className="text-white  items-center flex ">0</p>
+                </div>
+              </div>
+              <Image src={RobinoLogo} alt="" className="pr-2 w-[48px]" />
+            </div>
+            <div className="flex justify-between   rounded-lg bg-[#061230] w-[490px] pl-4  mt-2 mb-4 mr-4  items-center">
+              <div className="flex flex-col gap-x-2  py-[6px]">
+                <p className="text-white  items-center flex ">0.0000</p>
+                <div className="flex gap-x-1 items-center text-[14px] ">
+                  <p className="font-bold text-slate-500 flex items-center">
+                    USDM
+                  </p>
+                </div>
+              </div>
+              <Image src={USDM} alt="" className="pr-2 w-[48px]" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
