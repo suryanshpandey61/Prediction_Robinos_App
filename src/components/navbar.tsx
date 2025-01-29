ba01acc38415c0cf2fefc9281d671bd49989e119
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-01-30 01:22:58 +0530

    Navbar Updation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -306,7 +306,7 @@ function Navbar() {
       {/* Modal for wallet selection */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
-          <div className="bg-slate-900 text-white rounded-lg p-6 w-[350px]">
+          <div className="bg-slate-800 text-white rounded-lg p-6 w-[350px]">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-lg font-semibold">Connect Wallet</h2>
               <button
